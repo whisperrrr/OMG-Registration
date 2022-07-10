ad438d432bbd22fefd60eac8aeeb5c8c79f1a87f
whisperrrr <jh731448520@163.com>
2022-07-10 17:06:28 +0800

    feat: init the app.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+import './app.css'
+
+class App extends Component<any, any> {
+
+  componentDidMount () {}
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
